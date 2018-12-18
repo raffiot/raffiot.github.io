f143a3949adf6d4459bc89e6d86071adb8655395
raffiot <raphael.madillo@epfl.ch>
2018-12-18 09:38:19 +0100

    have all vis

---
+++ script.js
@@ -0,0 +1,28 @@
+var diseases_info = "<p>This dataset is composed by every wikipedia pages that are at depth 2 from disease wikipedia page. <br />"
+					+"Wikipedia pages: 1.385 <br /> "
+					+"Hyperlinks: 20.460</p> <br/>"
+					+"<a href='./diseases/viz.html' rel='noopener noreferrer' target='_blank'>Visualise ! </a>";
+					
+var wikispedia_info = "<p>This dataset is composed by science pages that are part of wikispedia dataset* <br />"
+					+"Wikipedia pages: 1.097 <br /> "
+					+"Hyperlinks: 13.971</p>";
+
+var century_info = "<p>This dataset is composed by every wikipedia pages that are at depth 2 from XX century wikipedia page <br />"
+					+"Wikipedia pages: 824 <br /> "
+					+"Hyperlinks: 11.320</p> <br/>";
+/* document.querySelector("input#visus").addEventListener("change",selectVis)
+function selectVis(){
+	var choosed_vis = document.querySelector("input#visus").value;
+	var info = document.querySelector("div#vis-info");
+	switch(choosed_vis){
+		case "Wikipedia diseases":
+			info.innerHTML = diseases_info;
+			break;
+		case "Wikipedia science":
+			info.innerHTML = wikispedia_info;
+			break;
+		case "Wikipedia XX century":
+			info.innerHTML = century_info;
+			break;
+	}
+} */
